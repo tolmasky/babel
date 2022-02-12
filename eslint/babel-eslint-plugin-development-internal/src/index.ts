0a9bcfb8e6e5b8fe1183c72ad210ef10c2ad4731
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2022-02-11 18:39:29 -0800

    Fix a bunch of errors messages.
    Reviewed by @tolmasky.

--- eslint/babel-eslint-plugin-development-internal/src/index.ts
+++ eslint/babel-eslint-plugin-development-internal/src/index.ts
@@ -1,8 +1,6 @@
-import dryErrorMessages from "./rules/dry-error-messages";
 import reportErrorMessageFormat from "./rules/report-error-message-format";
 
 export const rules = {
-  "dry-error-messages": dryErrorMessages,
   "report-error-message-format": reportErrorMessageFormat,
 };
 
